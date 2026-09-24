7c0ebc2762b72a17585f78ceb6b372b24f6e249a
sunpaws360-afk <sunpaws360@gmail.com>
2026-09-25 05:00:38 +0700

    chore: bump version to 1.3.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.rebecca.sunplayer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.3.0.1"
     }
 
     buildFeatures {
