196c2a48eac5cd38687af99e42a52dbde72c6d0b
sunpaws360-afk <sunpaws360@gmail.com>
2026-09-25 04:51:18 +0700

    chore(build): upgrade AGP for Android 16

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Root build.gradle.kts
 plugins {
-    id("com.android.application") version "8.5.0" apply false
+    id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
 }
